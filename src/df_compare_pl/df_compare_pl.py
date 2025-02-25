87761ab27385ba485e9de08ba608c5c934394d89
jake greenhalgh <1jake.g@gmail.com>
2025-02-25 09:27:30 -0700

    add col_only option

--- src/df_compare_pl/df_compare_pl.py
+++ src/df_compare_pl/df_compare_pl.py
@@ -1,6 +1,6 @@
 import polars as pl
 
-def df_compare(a:pl.DataFrame, b:pl.DataFrame, write_csv:bool=False) -> bool:
+def df_compare(a:pl.DataFrame, b:pl.DataFrame, col_only:bool=False, write_csv:bool=False) -> bool:
     """
     compares two polars dataframes for equality
     `equality` in this case means that:
@@ -13,6 +13,7 @@ def df_compare(a:pl.DataFrame, b:pl.DataFrame, write_csv:bool=False) -> bool:
     args:
         `a`: a pl.Dataframe
         `b`: a pl.Dataframe
+        `col_only`: a boolean indicating whether only the columns should be checked
         `write_csv`: a boolean indicating whether or not to write csvs with the 
         row differences, this is helpful when there are many row differences
         and the normal prints won't contain everything, defaults to `False`
@@ -32,29 +33,30 @@ def df_compare(a:pl.DataFrame, b:pl.DataFrame, write_csv:bool=False) -> bool:
         if dif_ba:
             print(f'col(s) in b but not a: {dif_ba}')
         return False
-    anti_ab = (
-        a.join(b, how='anti', on=a.columns, coalesce=True)
-    )
-    anti_ba = (
-        b.join(a, how='anti', on=b.columns, coalesce=True)
-    )
-    if not (anti_ab.is_empty() and anti_ba.is_empty()):
-        print('rows are different:')
-        if not anti_ab.is_empty():
-            print('row(s) in a but not b:')
-            print(anti_ab)
-            if write_csv:
-                fn1 = 'a_not_b.csv'
-                anti_ab.write_csv(fn1)
-                print(f'{fn1} written')
-        if not anti_ba.is_empty():
-            print('row(s) in b but not a:')
-            print(anti_ba)
-            if write_csv:
-                fn2 = 'b_not_a.csv'
-                anti_ba.write_csv(fn2)
-                print(f'{fn2} written')
-        return False
+    if not col_only:
+        anti_ab = (
+            a.join(b, how='anti', on=a.columns, coalesce=True)
+        )
+        anti_ba = (
+            b.join(a, how='anti', on=b.columns, coalesce=True)
+        )
+        if not (anti_ab.is_empty() and anti_ba.is_empty()):
+            print('rows are different:')
+            if not anti_ab.is_empty():
+                print('row(s) in a but not b:')
+                print(anti_ab)
+                if write_csv:
+                    fn1 = 'a_not_b.csv'
+                    anti_ab.write_csv(fn1)
+                    print(f'{fn1} written')
+            if not anti_ba.is_empty():
+                print('row(s) in b but not a:')
+                print(anti_ba)
+                if write_csv:
+                    fn2 = 'b_not_a.csv'
+                    anti_ba.write_csv(fn2)
+                    print(f'{fn2} written')
+            return False
     return True
 
 
